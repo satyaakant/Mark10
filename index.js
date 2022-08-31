1807e1464101e76aeb19fc224e06f7c5ee20db19
satyaaa <satyakantmishra50@gmail.com>
2022-09-01 04:16:54 +0530

    completed with css

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const billAmount=document.querySelector("#bill-amount");
 const cashGiven=document.querySelector("#cash-given");
 const checkButton= document.querySelector("#check-button");
 const message= document.querySelector("#error-message");
-const NoOfnotes=document.querySelector(".no-of-notes")
-const availablenotes = [2000, 500, 100, 50, 20, 10, 5, 1]
+const divTable=document.querySelector(".change-table")
+const NoOfnotes= document.querySelectorAll(".no-of-notes");
+const availablenotes = [2000, 500, 100, 10, 5, 1];
 
 checkButton.addEventListener("click", function validatebillandcashamount(){
     hideMessage();
@@ -27,6 +28,7 @@ function changeCalculator(cashtobereturnde){
         );
         cashtobereturnde %= availablenotes[i];
         NoOfnotes[i].innerText = numberOfNotes;
+        divTable.style.display="block"
     }
 }
 function showMessage(msg){
