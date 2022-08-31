bc93b2d4ebdae6ca1adede5c8d03cee4e9198f7c
satyaaa <satyakantmishra50@gmail.com>
2022-09-01 04:44:37 +0530

    css

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const billAmount=document.querySelector("#bill-amount");
 const cashGiven=document.querySelector("#cash-given");
 const checkButton= document.querySelector("#check-button");
 const message= document.querySelector("#error-message");
-const divTable=document.querySelector(".change-table")
+
 const NoOfnotes= document.querySelectorAll(".no-of-notes");
 const availablenotes = [2000, 500, 100, 10, 5, 1];
 
@@ -28,7 +28,7 @@ function changeCalculator(cashtobereturnde){
         );
         cashtobereturnde %= availablenotes[i];
         NoOfnotes[i].innerText = numberOfNotes;
-        divTable.style.display="block"
+      
     }
 }
 function showMessage(msg){
