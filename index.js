ab14fd8836c9ae29651084ba26d81643c3bf16ae
satyaaa <satyakantmishra50@gmail.com>
2022-08-31 17:21:56 +0530

    js

--- index.js
+++ index.js
@@ -2,17 +2,37 @@ const billAmount=document.querySelector("#bill-amount");
 const cashGiven=document.querySelector("#cash-given");
 const checkButton= document.querySelector("#check-button");
 const message= document.querySelector("#error-message");
+const NoOfnotes=document.querySelector(".no-of-notes")
+const availablenotes = [2000, 500, 100, 50, 20, 10, 5, 1]
 
 checkButton.addEventListener("click", function validatebillandcashamount(){
-    message.style.display = "none";
+    hideMessage();
     if(billAmount.value>0){
-
+        if(cashGiven.value >= billAmount.value){
+          const cashtobereturnde = cashGiven.value - billAmount.value;
+            changeCalculator(cashtobereturnde);
+        }
+        else{
+            showMessage("Cash given must be equal to Bill amount");
+        }
     }
     else{
         showMessage("invalid bill amount");
     }
 });
-function showMessage(message){
+function changeCalculator(cashtobereturnde){
+    for(let i = 0 ; i < availablenotes.length ; i++){
+        const numberOfNotes = Math.trunc(
+            cashtobereturnde / availablenotes[i]
+        );
+        cashtobereturnde %= availablenotes[i];
+        NoOfnotes[i].innerText = numberOfNotes;
+    }
+}
+function showMessage(msg){
     message.style.display = "block";
-    message.innerText = message;
+    message.innerText = msg;
+}
+function hideMessage(){
+    message.style.display = "none";
 }
